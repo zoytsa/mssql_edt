91bbe783bf70c7e792a5fd9759237b2a584a8dae
Batursky <a.baturskiy@gmail.com>
2023-10-08 14:03:51 +0600

    sync python core ok

--- core-python/sync-run-copy.py
+++ core-python/sync-run-copy.py
@@ -7,7 +7,7 @@ current_directory = os.getcwd()
 source_path = os.path.join(current_directory, "mssql8", "src", "catalogs", "tes", "tes.mdo")
 # print(source_path)
 
-destination_path = os.path.join(current_directory, "python-core", "metadata-inner-sync", "catalog_metedata_tes5.mdo")
+destination_path = os.path.join(current_directory, "core-python", "metadata-inner-sync", "catalog_metedata_tes74475.mdo")
 print(destination_path)
 
 
